4b1ede240868b936fa6ae43873c13da02d3bc8f2
Santos <alanjosedossantos011@gmail.com>
2021-03-12 09:38:07 -0300

    Service cliente finalizado

---
+++ src/main/java/com/acc/banco/service/ClienteService.java
@@ -0,0 +1,60 @@
+package com.acc.banco.service;
+
+import ch.qos.logback.core.net.server.Client;
+import com.acc.banco.model.Cliente;
+import com.acc.banco.repository.ClienteRepository;
+import com.acc.banco.service.exception.DataIntegrityViolationException;
+import com.acc.banco.service.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    //buscar todos os clientes
+    public List<Cliente> findAll (){
+
+        return clienteRepository.findAll();
+    }
+
+    //buscar clientes por ID
+    public Cliente findId (Long id) {
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+
+        return cliente.orElseThrow(() -> new ObjectNotFoundException(
+                "Código não encontrado! Id: " + id ));
+    }
+
+    //salvanod cliente.
+    public Cliente save (Cliente cliente) {
+
+        return clienteRepository.save(cliente);
+    }
+
+    //realizando update do cliente
+    public Cliente update (Cliente cliente){
+        Cliente newCliente = findId(cliente.getIdCliente());
+        updateData(newCliente, cliente);
+        return clienteRepository.save(cliente);
+    }
+
+    public Cliente delete (Long id){
+        Cliente cliente = findId(id);
+        clienteRepository.deleteById(cliente.getIdCliente());
+
+        return cliente;
+
+    }
+
+    //Metodo para atualizar os campos que serão realizado o update.
+    private void updateData (Cliente newCliente, Cliente cliente){
+        newCliente.setFone(cliente.getFone());
+        newCliente.setClienteCPF(cliente.getClienteCPF());
+    }
+}
--- src/main/java/com/acc/banco/service/exception/ObjectNotFoundException.java
+++ src/main/java/com/acc/banco/service/exception/ObjectNotFoundException.java
@@ -3,6 +3,10 @@ package com.acc.banco.service.exception;
 public class ObjectNotFoundException extends RuntimeException{
     private static final long serialVersionUID = 1L;
 
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+
     public ObjectNotFoundException(String msg, Throwable cause){
         super(msg, cause);
     }
